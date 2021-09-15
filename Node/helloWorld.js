bdcaf1d366b561df50adfd0ace84bf85d7f14832
KimJiMin <minmin4736@naver.com>
2021-09-15 21:40:25 +0900

    📑 :: test

---
+++ Node/helloWorld.js
@@ -0,0 +1,17 @@
+function helloWorld() {
+	console.log('Hello World');
+	helloNode();
+}
+
+function helloNode() {
+	console.log('Hello Node');
+}
+
+helloWorld();
+
+//REPL에 직접 코드를 입력하는 대신 자바스크립트 파일을 만들어 실행.
+//REPL
+//R: Read(읽기)
+//E: Eval(해석)
+//P: Print(출력)
+//L: Loop(반복)
